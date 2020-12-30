21865df5e73c5c559a0f5bd57850a007cfd211d7
aniket091 <75978424+aniket091@users.noreply.github.com>
2020-12-30 21:08:18 +0530

    Update react-on-ping.py

--- react-on-ping/react-on-ping.py
+++ react-on-ping/react-on-ping.py
@@ -3,12 +3,7 @@ from discord.ext import commands
 
 class ReactOnPing(commands.Cog):
     """Reacts with a ping emoji when someone gets pinged."""
-    emojis = [
-    "🇵",
-    "🇮",
-    "🇳",
-    "🇬"
-    ]
+    emojis =
 
     def __init__(self, bot):
         self.bot = bot
