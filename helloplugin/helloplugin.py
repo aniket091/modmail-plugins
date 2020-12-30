33b907378cb30229fa2e849c31bbf1d20806372e
aniket091 <75978424+aniket091@users.noreply.github.com>
2020-12-30 10:47:50 +0530

    Create helloplugin.py

---
+++ helloplugin/helloplugin.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+class HelloPlugin(commands.Cog):
+    def __init__ (self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+
+        if message.author.bot:
+            return
+
+        if "hello" in message.content.lower():
+            await message.channel.send("Hey")
+        elif "gm" in message.content.lower():
+            await message.channel.send("Good Morning")
+        elif "gn" in message.content.lower():
+            await message.channel.send("Good Night")
+        elif "good morning" in message.content.lower():
+            await message.channel.send("Good Morning !")
+        elif "good night" in message.content.lower():
+            await message.channel.send("Good Night !")
+
+def setup(bot):
+    bot.add_cog(HelloPlugin(bot))
