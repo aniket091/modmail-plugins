9d27484bad28d62522c76a8a7e6db79ec1391893
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-09 16:22:59 +0530

    Update ChangeStatus.py

--- ChangeStatus/ChangeStatus.py
+++ ChangeStatus/ChangeStatus.py
@@ -15,7 +15,7 @@ class ChangeStatus(commands.Cog):
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{self.second}"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{self.third}"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{self.third}"))
         await asyncio.sleep(10)
 
     @commands.group(name="statusy", invoke_without_command=True)
