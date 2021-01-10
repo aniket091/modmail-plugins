1d45848d61569b699aea2b72d96969e3862ab73b
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-10 10:52:49 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 class staff(commands.Cog):
     def __init__(self, bot):
@@ -8,17 +9,7 @@ class staff(commands.Cog):
     @commands.command()
     async def online(self, ctx):
         await ctx.message.delete()
-        await ctx.send(f"{ctx.author.mention}, reporting 10-41 <:online:797692836911906816>")
-        
-    @commands.command()
-    async def offline(self, ctx):
-        await ctx.message.delete()
-        await ctx.send(f"{ctx.author.mention}, reporting 10-42 <:dnd:797692836745183232>")  
-        
-    @commands.command()
-    async def break(self, ctx):
-        await ctx.message.delete()
-        await ctx.send(f"{ctx.author.mention}, reporting 10-7 <:idle:797695058207178753>")         
+        await ctx.send(f"{ctx.author.mention}, reporting 10-41 <:online:797692836911906816>")       
 
         
 def setup(bot):
