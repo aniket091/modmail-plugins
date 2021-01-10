c13f6e8d21a8402f3cef14c1d5a77c19a56c0e38
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-10 11:18:33 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -15,12 +15,7 @@ class staff(commands.Cog):
     async def offline(self, ctx):
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-42 <:dnd:797692836745183232>")
-        
-    @commands.command()
-    async def break(self, ctx):
-        await ctx.message.delete()
-        await ctx.send(f"{ctx.author.mention}, reporting 10-7 <:idle:797695058207178753>") 
-        
+                
         
 def setup(bot):
     bot.add_cog(staff(bot))
