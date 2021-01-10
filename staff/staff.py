b7de7c896abad958a34c2e268285198edda6517b
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-10 10:45:48 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -5,9 +5,20 @@ class staff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    @commands.command(aliases=['n'])
     async def online(self, ctx):
-        await ctx.send(f"{ctx.author.mention}, reporting online(10-41) <:online:797692836911906816>")
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.author.mention}, reporting 10-41 <:online:797692836911906816>")
+        
+    @commands.command(aliases=['f'])
+    async def offline(self, ctx):
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.author.mention}, reporting 10-42 <:dnd:797692836745183232>")  
+        
+    @commands.command(aliases=['b'])
+    async def break(self, ctx):
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.author.mention}, reporting 10-7 <:idle:797695058207178753>")     
         
         
 def setup(bot):
