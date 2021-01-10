d9d6263673d47fc91a36d09f364566c07782127d
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-10 10:49:30 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -5,7 +5,7 @@ class staff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(aliases=['n'])
+    @commands.command()
     async def online(self, ctx):
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-41 <:online:797692836911906816>")
