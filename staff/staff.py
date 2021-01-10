ea35bff32ce4ac9b823e6227ab4541febe4d5f13
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-10 10:16:14 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -7,7 +7,7 @@ class staff(commands.Cog):
     
     @commands.command()
     async def online(self, ctx):
-        await ctx.send(f"{message.author.mention}")
+        await ctx.send(f"{0.author.mention}")
         
         
 def setup(bot):
