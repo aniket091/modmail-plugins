acd247b58cfe56953a7ae90c26dac25fcb23c7ad
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-11 09:44:12 +0530

    Update training.py

--- training/training.py
+++ training/training.py
@@ -37,7 +37,6 @@ class Training(commands.Cog):
             
     @commands.command(aliases=["train"])
     @checks.has_permissions(PermissionLevel.OWNER)
-    @commands.cooldown(1, 1800, commands.BucketType.user)
     async def training(self, ctx):
         """Host a training."""
         config = await self.db.find_one({"_id": "config"})
@@ -49,20 +48,12 @@ class Training(commands.Cog):
         except KeyError:
             training_mention = ""
         
-        embed = discord.Embed(description="Salutations, a training is currently being hosted at the Training Center! Come to the Training Center for a chance of a promotion!", timestamp=datetime.datetime.utcnow())
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed = discord.Embed(description=f"{ctx.author.mention} <:online:797692836911906816>", timestamp=datetime.datetime.utcnow())
         embed.color = self.bot.main_color
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/633681171879952384/767355349870182430/image0.png")
-        embed.set_footer(text="Vinns Hotel")
-        
-        embed.add_field(name="Host:", value=f"{ctx.author.mention} | {ctx.author.name}#{ctx.author.discriminator} | {ctx.author.nick}", inline=False)
-        embed.add_field(name="Session Status:", value=f"Open", inline=False)
-        embed.add_field(name="Training Link:", value=f"Click [here](https://www.roblox.com/games/4780049434).", inline=False)
 
         msggg = await setchannel.send(training_mention, embed=embed)
         asyncio.sleep(5)
         await msggg.edit(content=f"{training_mention} | msgID: {msggg.id}", embed=embed)
-        await ctx.send("<a:check:742680789262663710> | Training announcement has been posted!")
             
     @commands.command(aliases=["et"])
     @checks.has_permissions(PermissionLevel.OWNER)
@@ -80,13 +71,12 @@ class Training(commands.Cog):
         except:
             embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
             await ctx.send(embed=embed)
-        embed2=discord.Embed(title="Vinns Hotel Trainings", description=f"The training by **{ctx.author.mention} | {ctx.author.name}#{ctx.author.discriminator}** has concluded! Please be on a look out for a future training.", color=0xe74c3c)
-        embed2.set_thumbnail(url="https://cdn.discordapp.com/attachments/633681171879952384/767355349870182430/image0.png")
-        embed2.set_footer(text="Vinns Hotel")
+        embed2=discord.Embed(description=f"{ctx.author.mention} <:dnd:797692836745183232>", color=0xe74c3c)
+        embed2.color = self.bot.main_color
         await message.edit(embed=embed2, content=training_mention) # <@&695243187043696650>
         
         await ctx.send("<a:check:742680789262663710> | Training announcement has been edited and the training has ended!")
-        await asyncio.sleep(600)
+        await asyncio.sleep(5)
         await message.delete()
             
 def setup(bot):
