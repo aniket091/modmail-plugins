e68a0c6aea95c3c13d1d8d77d07caaac2356cc49
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-11 13:20:17 +0530

    Update training.py

--- training/training.py
+++ training/training.py
@@ -103,6 +103,29 @@ class Training(commands.Cog):
         
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-7 <:idle:797695058207178753>")
+        
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.OWNER)
+    async def teneight(self, ctx, *, msgID: str):
+        """back for staff."""
+        config = await self.db.find_one({"_id": "config"})
+        channel = self.bot.get_channel(config["training_channel"])
+        try:
+            training_mention = config["training_mention"]
+        except KeyError:
+            training_mention = ""
+        try: 
+            msgID: int(msgID)
+            message = await channel.fetch_message(msgID)
+        except:
+            embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
+            await ctx.send(embed=embed)
+        embed4=discord.Embed(description=f"{ctx.author.mention}\n**__Status__**\n**Back (10-8)** <:streaming:798080684778061835>", color=0x009dff)
+        await message.edit(embed=embed4) # <@&695243187043696650>
+        
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.author.mention}, reporting back 10-8 <:streaming:798080684778061835><:online:797692836911906816>")
+
 
             
 def setup(bot):
