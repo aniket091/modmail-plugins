104d5c8ccd8a9d8b740be271820d3c94f7785026
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-11 22:00:38 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -100,6 +100,8 @@ class staff(commands.Cog):
         embed3=discord.Embed(description=f"**__Status__**\n**Offline** <:dnd:797692836745183232>", color=0xFF0000, timestamp=datetime.datetime.utcnow())
         embed3.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         await message.edit(embed=embed3, content=training_mention) # <@&695243187043696650>
+        asyncio.sleep(20)
+        await message.edit(embed=embed6, content=training_mention)
         
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-42 <:dnd:797692836745183232>")
@@ -146,9 +148,34 @@ class staff(commands.Cog):
         embed5=discord.Embed(description=f"**__Status__**\n**Back (10-8)** <:streaming:798080684778061835>", color=0x9900cc, timestamp=datetime.datetime.utcnow())
         embed5.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         await message.edit(embed=embed5, content=training_mention) # <@&695243187043696650>
+        asyncio.sleep(20)
+        await message.edit(embed=embed2, content=training_mention)
         
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-8 <:streaming:798080684778061835>")
+        
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def tennight(self, ctx, *, msgID: str):
+        """godnight for staff."""
+        config = await self.db.find_one({"_id": "config"})
+        channel = self.bot.get_channel(config["training_channel"])
+        try:
+            training_mention = config["training_mention"]
+        except KeyError:
+            training_mention = ""
+        try: 
+            msgID: int(msgID)
+            message = await channel.fetch_message(msgID)
+        except:
+            embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
+            await ctx.send(embed=embed)
+        embed6=discord.Embed(description=f"**__Status__**\n**Offline** <:invisible:798080684991971348>", color=0x009dff, timestamp=datetime.datetime.utcnow())
+        embed6.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        await message.edit(embed=embed6, content=training_mention) # <@&695243187043696650>
+        
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.author.mention}, reporting bye bye <:invisible:798080684991971348>")
 
 
             
