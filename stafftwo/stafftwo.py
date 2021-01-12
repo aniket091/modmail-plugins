0a50f6872151dec1b60feb113fd748f70c7c8c04
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-12 12:13:13 +0530

    Update stafftwo.py

--- stafftwo/stafftwo.py
+++ stafftwo/stafftwo.py
@@ -97,9 +97,9 @@ class stafftwo(commands.Cog):
         except:
             embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
             await ctx.send(embed=embed)
-        embed3=discord.Embed(description=f"**__Status__**\n**Offline** <:dnd:797692836745183232>", color=0xFF0000, timestamp=datetime.datetime.utcnow())
+        embed3=discord.Embed(description=f"**__Status__**\n**Offline** <:invisible:798080684991971348>", color=0x009dff, timestamp=datetime.datetime.utcnow())
         embed3.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        await asyncio.sleep(10)
+        await asyncio.sleep(5)
         await message.edit(embed=embed3, content=training_mention) # <@&695243187043696650>
         
         await ctx.message.delete()
