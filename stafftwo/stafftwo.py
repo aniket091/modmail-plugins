85cfd86c02880eb13912d0a037a68d405d308462
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-12 12:22:25 +0530

    Update stafftwo.py

--- stafftwo/stafftwo.py
+++ stafftwo/stafftwo.py
@@ -125,51 +125,6 @@ class stafftwo(commands.Cog):
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-7 <:idle:797695058207178753>")
         
-    @commands.command(aliases=["10-8"])
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def teneighttwo(self, ctx, *, msgID: str):
-        """back for staff."""
-        config = await self.db.find_one({"_id": "config"})
-        channel = self.bot.get_channel(config["training_channel"])
-        try:
-            training_mention = config["training_mention"]
-        except KeyError:
-            training_mention = ""
-        try: 
-            msgID: int(msgID)
-            message = await channel.fetch_message(msgID)
-        except:
-            embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
-            await ctx.send(embed=embed)
-        embed5=discord.Embed(description=f"**__Status__**\n**Online** <:online:797692836911906816>", color=0x00ff00, timestamp=datetime.datetime.utcnow())
-        embed5.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        await asyncio.sleep(5)
-        await message.edit(embed=embed5, content=training_mention) # <@&695243187043696650>
-        
-    @commands.command()
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def tennight(self, ctx, *, msgID: str):
-        """godnight for staff."""
-        config = await self.db.find_one({"_id": "config"})
-        channel = self.bot.get_channel(config["training_channel"])
-        try:
-            training_mention = config["training_mention"]
-        except KeyError:
-            training_mention = ""
-        try: 
-            msgID: int(msgID)
-            message = await channel.fetch_message(msgID)
-        except:
-            embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
-            await ctx.send(embed=embed)
-        embed6=discord.Embed(description=f"**__Status__**\n**Offline** <:invisible:798080684991971348>", color=0x009dff, timestamp=datetime.datetime.utcnow())
-        embed6.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        await message.edit(embed=embed6, content=training_mention) # <@&695243187043696650>
-        
-        await ctx.message.delete()
-        await ctx.send(f"{ctx.author.mention}, reporting bye bye <:invisible:798080684991971348>")
-
-
-            
+             
 def setup(bot):
     bot.add_cog(stafftwo(bot))
