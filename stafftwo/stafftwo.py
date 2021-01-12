7cd75b1b1ee21fd68bda565c01ac18290ea87e40
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-12 12:08:40 +0530

    Update stafftwo.py

--- stafftwo/stafftwo.py
+++ stafftwo/stafftwo.py
@@ -13,9 +13,9 @@ class stafftwo(commands.Cog):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
     
-    @commands.command(aliases=["tchannel"])
+    @commands.command()
     @checks.has_permissions(PermissionLevel.OWNER)
-    async def trainingchannel(self, ctx, channel: discord.TextChannel):
+    async def trainingchannelfalse(self, ctx, channel: discord.TextChannel):
         """Set the training channel!"""
         await self.db.find_one_and_update({"_id": "config"}, {"$set": {"training_channel": channel.id}}, upsert=True)
         
@@ -24,9 +24,9 @@ class stafftwo(commands.Cog):
         
         await ctx.send(embed=embed)
 
-    @commands.command(aliases=["tmention"])
+    @commands.command()
     @checks.has_permissions(PermissionLevel.OWNER)
-    async def trainingmention(self, ctx, *, mention: str):
+    async def trainingmentionfalse(self, ctx, *, mention: str):
         """Sets the training mention"""
         await self.db.find_one_and_update({"_id": "config"}, {"$set": {"training_mention": mention}}, upsert=True)
         
@@ -35,9 +35,9 @@ class stafftwo(commands.Cog):
         
         await ctx.send(embed=embed)
             
-    @commands.command(aliases=["tnew"])
+    @commands.command()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def embednew(self, ctx):
+    async def embednewfalse(self, ctx):
         """make a new status message."""
         config = await self.db.find_one({"_id": "config"})
         training_channel = config["training_channel"]
@@ -58,9 +58,9 @@ class stafftwo(commands.Cog):
         await ctx.message.delete()
         await ctx.send(f"<:yes:793742648141545482> you have created a new status message")
             
-    @commands.command(aliases=["n"])
+    @commands.command()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def online(self, ctx, *, msgID: str):
+    async def onlinefalse(self, ctx, *, msgID: str):
         """be online"""
         config = await self.db.find_one({"_id": "config"})
         channel = self.bot.get_channel(config["training_channel"])
@@ -81,9 +81,9 @@ class stafftwo(commands.Cog):
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-41 <:online:797692836911906816>")
         
-    @commands.command(aliases=["f"])
+    @commands.command(aliases=["offline"])
     @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def offlineasad(self, ctx, *, msgID: str):
+    async def offlinetwo(self, ctx, *, msgID: str):
         """offline for staff."""
         config = await self.db.find_one({"_id": "config"})
         channel = self.bot.get_channel(config["training_channel"])
@@ -100,14 +100,14 @@ class stafftwo(commands.Cog):
         embed3=discord.Embed(description=f"**__Status__**\n**Offline** <:dnd:797692836745183232>", color=0xFF0000, timestamp=datetime.datetime.utcnow())
         embed3.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         await asyncio.sleep(10)
-        await message.edit(embed=embed6, content=training_mention) # <@&695243187043696650>
+        await message.edit(embed=embed3, content=training_mention) # <@&695243187043696650>
         
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-42 <:dnd:797692836745183232>")
         
-    @commands.command(aliases=["10-7"])
+    @commands.command()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def tenseven(self, ctx, *, msgID: str):
+    async def tensevenfalse(self, ctx, *, msgID: str):
         """break for staff."""
         config = await self.db.find_one({"_id": "config"})
         channel = self.bot.get_channel(config["training_channel"])
@@ -130,7 +130,7 @@ class stafftwo(commands.Cog):
         
     @commands.command(aliases=["10-8"])
     @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def teneight(self, ctx, *, msgID: str):
+    async def teneighttwo(self, ctx, *, msgID: str):
         """back for staff."""
         config = await self.db.find_one({"_id": "config"})
         channel = self.bot.get_channel(config["training_channel"])
@@ -151,29 +151,6 @@ class stafftwo(commands.Cog):
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-8 <:streaming:798080684778061835>")
         
-    @commands.command()
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def tennight(self, ctx, *, msgID: str):
-        """godnight for staff."""
-        config = await self.db.find_one({"_id": "config"})
-        channel = self.bot.get_channel(config["training_channel"])
-        try:
-            training_mention = config["training_mention"]
-        except KeyError:
-            training_mention = ""
-        try: 
-            msgID: int(msgID)
-            message = await channel.fetch_message(msgID)
-        except:
-            embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
-            await ctx.send(embed=embed)
-        embed6=discord.Embed(description=f"**__Status__**\n**Offline** <:invisible:798080684991971348>", color=0x009dff, timestamp=datetime.datetime.utcnow())
-        embed6.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        await message.edit(embed=embed6, content=training_mention) # <@&695243187043696650>
-        
-        await ctx.message.delete()
-        await ctx.send(f"{ctx.author.mention}, reporting bye bye <:invisible:798080684991971348>")
-
 
             
 def setup(bot):
