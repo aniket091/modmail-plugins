b48681583da089b68eb388636d357804b0af18fe
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-12 10:14:58 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -81,9 +81,9 @@ class staff(commands.Cog):
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-41 <:online:797692836911906816>")
         
-    @commands.command(aliases=["f"])
+    @commands.command(aliases=["offline"])
     @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def offline(self, ctx, *, msgID: str):
+    async def oofflinetwo(self, ctx, *, msgID: str):
         """offline for staff."""
         config = await self.db.find_one({"_id": "config"})
         channel = self.bot.get_channel(config["training_channel"])
@@ -97,8 +97,9 @@ class staff(commands.Cog):
         except:
             embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
             await ctx.send(embed=embed)
-        embed3=discord.Embed(description=f"**__Status__**\n**Offline** <:dnd:797692836745183232>", color=0xFF0000, timestamp=datetime.datetime.utcnow())
+        embed3=discord.Embed(description=f"**__Status__**\n**Online** <:online:797692836911906816>", color=0x00ff00, timestamp=datetime.datetime.utcnow())
         embed3.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        asyncio.sleep(15)
         await message.edit(embed=embed3, content=training_mention) # <@&695243187043696650>
         
         await ctx.message.delete()
