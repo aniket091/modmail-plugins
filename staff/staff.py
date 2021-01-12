c6b46ed92bae3136905bfcbf9abc23520d202874
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-12 18:32:01 +0530

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -74,14 +74,12 @@ class staff(commands.Cog):
         except:
             embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
             await ctx.send(embed=embed)
-        embed2=discord.Embed(description=f"**__Status__**\n**Online** <:online:797692836911906816>", color=0x009dff, timestamp=datetime.datetime.utcnow())
+        embed2=discord.Embed(description=f"**__Status__**\n**Online** <:online:797692836911906816>", color=0x00e600, timestamp=datetime.datetime.utcnow())
         embed2.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         await message.edit(embed=embed2, content=training_mention) # <@&695243187043696650>
         
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-41 <:online:797692836911906816>")
-        await asyncio.sleep(5)
-        await message.edit(embed=embed2, content=training_mention)
         
     @commands.command(aliases=["f"])
     @checks.has_permissions(PermissionLevel.SUPPORTER)
@@ -99,12 +97,16 @@ class staff(commands.Cog):
         except:
             embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
             await ctx.send(embed=embed)
-        embed3=discord.Embed(description=f"**__Status__**\n**Offline** <:dnd:797692836745183232>", color=0xFF0000, timestamp=datetime.datetime.utcnow())
+        embed3=discord.Embed(description=f"**__Status__**\n**Offline** <:dnd:797692836745183232>", color=0xfa1313, timestamp=datetime.datetime.utcnow())
         embed3.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed7=discord.Embed(description=f"**__Status__**\n**Offline** <:invisible:798080684991971348>", color=0xa9a9a9, timestamp=datetime.datetime.utcnow())
+        embed7.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         await message.edit(embed=embed3, content=training_mention) # <@&695243187043696650>
         
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-42 <:dnd:797692836745183232>")
+        await asyncio.sleep(5)
+        await message.edit(embed=embed7, content=training_mention)
         
     @commands.command(aliases=["10-7"])
     @checks.has_permissions(PermissionLevel.SUPPORTER)
@@ -147,40 +149,15 @@ class staff(commands.Cog):
             await ctx.send(embed=embed)
         embed5=discord.Embed(description=f"**__Status__**\n**Back (10-8)** <:streaming:798080684778061835>", color=0x9900cc, timestamp=datetime.datetime.utcnow())
         embed5.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        embed7=discord.Embed(description=f"**__Status__**\n**Online** <:online:797692836911906816>", color=0x009dff, timestamp=datetime.datetime.utcnow())
-        embed7.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed6=discord.Embed(description=f"**__Status__**\n**Online** <:online:797692836911906816>", color=0x00ff00, timestamp=datetime.datetime.utcnow())
+        embed6.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         await message.edit(embed=embed5, content=training_mention) # <@&695243187043696650>
         
         await ctx.message.delete()
         await ctx.send(f"{ctx.author.mention}, reporting 10-8 <:streaming:798080684778061835>")
         await asyncio.sleep(5)
-        await message.edit(embed=embed7, content=training_mention)
-        
-    @commands.command(aliases=["bye"])
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def tennight(self, ctx, *, msgID: str):
-        """godnight for staff."""
-        config = await self.db.find_one({"_id": "config"})
-        channel = self.bot.get_channel(config["training_channel"])
-        try:
-            training_mention = config["training_mention"]
-        except KeyError:
-            training_mention = ""
-        try: 
-            msgID: int(msgID)
-            message = await channel.fetch_message(msgID)
-        except:
-            embed=discord.Embed(title="Please include a valid Message ID that is in the training channel.", description="[Where can I find a Message ID?](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)", color=0xe74c3c)
-            await ctx.send(embed=embed)
-        embed6=discord.Embed(description=f"**__Status__**\n**Offline** <:invisible:798080684991971348>", color=0x009dff, timestamp=datetime.datetime.utcnow())
-        embed6.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        await asyncio.sleep(20)
-        await message.edit(embed=embed6, content=training_mention) # <@&695243187043696650>
-        
-        await ctx.message.delete()
-        await ctx.send(f"{ctx.author.mention}, reporting bye bye <:invisible:798080684991971348>")
+        await message.edit(embed=embed6, content=training_mention)
 
-
-            
+        
 def setup(bot):
     bot.add_cog(staff(bot))
