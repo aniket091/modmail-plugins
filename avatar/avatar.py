a5708b30f19a0c9b46dcc359fb39f946ed64bba1
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-13 09:45:10 +0530

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -7,17 +7,10 @@ class avatar(commands.Cog):
         self.bot = bot
         
     @commands.command()
-    async def avatar(self, ctx, member : discord.Member = None):
-        
-        if member is None:
-            embed = discord.Embed(title="This command is used like this ```!avatar [member]", color=0xff0000)
-            await ctx.send(embed=embed)
-            return
-            
-        else:
-            embed2 = discord.Embed(title=f"{member}'s Avatar!", color=0x00ff00)
-            embed2.set_image(url=member.avatar_url)
-            await ctx.send(embed=embed2)
+    async def avatar(self, ctx, *, member : discord.Member = None):
+        embed2 = discord.Embed(title=f"{member}'s Avatar!", color=0x00ff00)
+        embed2.set_image(url==member.avatar_url)
+        await ctx.send(embed=embed2)
 
 def setup(bot):
     bot.add_cog(avatar(bot))
