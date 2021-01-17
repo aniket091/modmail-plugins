fa810cb7a5acc88d3c427df4065a4c2691387ecd
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-17 10:09:51 +0530

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -9,16 +9,8 @@ class HelloPlugin(commands.Cog):
         if message.author.bot:
             return
 
-        if "hello" in message.content.startswith():
+        if message.content.startswith("hello"):
             await message.channel.send("Hey")
-        elif "gm" in message.content.lower():
-            await message.channel.send("Good Morning")
-        elif "gn" in message.content.lower():
-            await message.channel.send("Good Night")
-        elif "good morning" in message.content.lower():
-            await message.channel.send("Good Morning !")
-        elif "good night" in message.content.lower():
-            await message.channel.send("Good Night !")
 
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
