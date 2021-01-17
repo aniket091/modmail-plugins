e34687d06b315fb6f97d1930dd7be6f7f6d00683
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-17 11:05:35 +0530

    Update say.py

--- say/say.py
+++ say/say.py
@@ -6,7 +6,7 @@ class Say(commands.Cog):
         self.bot = bot
         
     @commands.command()
-    async def log(self, ctx, *, message=None):
+    async def reason(self, ctx, *, message=None):
         """Make the bot say something"""
         await ctx.message.delete()
         await ctx.send(message)
