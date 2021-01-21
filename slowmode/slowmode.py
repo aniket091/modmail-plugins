561d24ba0cd4b2d598696379aa24dd79de7add3a
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-22 00:03:11 +0530

    Update slowmode.py

--- slowmode/slowmode.py
+++ slowmode/slowmode.py
@@ -54,5 +54,30 @@ class SlowMode(commands.Cog):
         embed.set_author(name="Slow Mode")
         await ctx.send(embed=embed)
 
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_guild_permissions(manage_channels=True)
+    @commands.bot_has_guild_permissions(manage_channels=True)
+    async def lockdown(self, ctx, channel: discord.TextChannel=None):
+        channel = channel or ctx.channel
+        if ctx.guild.default_role not in channel.overwrites:
+            # This is the same as the elif except it handles agaisnt empty overwrites dicts
+            overwrites = {
+            ctx.guild.default_role: discord.PermissionOverwrite(send_messages=False)
+            }
+            await channel.edit(overwrites=overwrites)
+            await ctx.send(f"I have put {channel.name} on lockdown.")
+        elif channel.overwrites[ctx.guild.default_role].send_messages == True or channel.overwrites[ctx.guild.default_role].send_messages == None:
+            overwrites = channel.overwrites[ctx.guild.default_role]
+            overwrites.send_messages = False
+            await channel.set_permissions(ctx.guild.default_role, overwrite=overwrites)
+            await ctx.send(f"I have put {channel.name} on lockdown.")
+        else:
+            overwrites = channel.overwrites[ctx.guild.default_role]
+            overwrites.send_messages = True
+            await channel.set_permissions(ctx.guild.default_role, overwrite=overwrites)
+            await ctx.send(f"I have removed {channel.name} from lockdown.")
+
+
 def setup(bot):
     bot.add_cog(SlowMode(bot))
