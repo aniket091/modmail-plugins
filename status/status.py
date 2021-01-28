e6ee45a6aafa45c5487050d5ecdd1ffa5570ecf9
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-28 12:33:27 +0530

    Update status.py

--- status/status.py
+++ status/status.py
@@ -13,7 +13,7 @@ class Status(commands.Cog):
     async def start_the_status(self):
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"PARTH YT'S SERVER"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"-help"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"UNDER MAINTANCE | ANIKET NOOBDA"))
         await asyncio.sleep(10)
         server = self.bot.get_guild(665842728545943552)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} Members!"))
