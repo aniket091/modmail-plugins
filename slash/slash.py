dcc60084aec570e8d763288af4de3938f078fd03
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-02-28 21:25:48 +0530

    Update slash.py

--- slash/slash.py
+++ slash/slash.py
@@ -1,4 +1,4 @@
-  
+
 import discord
 import inspect
 import typing
@@ -8,78 +8,17 @@ from .model import CogCommandObject, CogSubcommandObject
 
 from random import randint
 client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(client, sync_commands=True)
+guild_ids = [664505860327997461]
 
 class slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @cog_ext.cog_slash(
-      name="rps",
-      description="TEST",
-      options=[manage_commands.create_option(
-        name = "rps",
-        description = "play rps with bot",
-        option_type = 3,
-        required = True,
-        choices =[
-       {
-         "name": "rock",
-         "value": "rock"
-       },{
-         "name": "paper",
-         "value": "paper"
-       },{
-         "name": "scissors",
-         "value": "scissors"
-       }]
-      )],
-    )
 
- 
+    @cog_ext.cog_slash(name="ping")
+    async def ping(self, ctx: SlashContext):
+        await ctx.send(content="Pong!")
 
-    async def _rps(ctx, rps: str):
-      choicelist = ["rock", "paper", "scissors"]
-      botval = choicelist[randint(0,2)]
-      async def winner(bot,user):
-        if(bot == 'scissors' and user == 'rock'):
-         await ctx.send(f"bot choose {botval} you won")
-        if(bot == 'rock' and user == 'paper'):
-          await ctx.send(f"bot choose {botval} you won")
-        if(bot == 'paper' and user == 'scissors'):
-          await ctx.send(f"bot choose {botval} you won")
-        if(bot == user):
-          await ctx.send(f"BOT CHOOSE {botval} its a tie") 
-        if(bot == 'rock' and user == 'scissors'):
-          await ctx.send(f"bot choose {botval} you lost") 
-        if(bot == 'paper' and user == 'rock'):
-          await ctx.send(f"bot choose {botval} you lost") 
-        if(bot == 'scissors' and user == 'paper'):
-          await ctx.send(f"bot choose {botval} you lost") 
-        await ctx.respond()
-      await winner(botval,rps)
-
-
-
-    @cog_ext.cog_slash(
-      name="test",
-      description="this returns the bot latency",
-      options=[manage_commands.create_option(
-        name = "title",
-        description = "description of first argument",
-        option_type = 3,
-        required = True
-      ),manage_commands.create_option(
-        name = "content",
-        description = "description of first argument",
-        option_type = 3,
-        required = True
-      )],
-      guild_ids=guild_ids
-    )
-    async def _test(ctx,content: str, title: str, color):
-        embed = discord.Embed(title=f"{title}", description=f"{content}")
-        await ctx.respond()
-        await ctx.send(embed = embed)
 
 def setup(bot):
     bot.add_cog(slash(bot))
