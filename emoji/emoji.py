01974ba7327a241469fcb25d211e33c82605b138
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-04-04 22:37:10 +0530

    Update emoji.py

--- emoji/emoji.py
+++ emoji/emoji.py
@@ -10,8 +10,10 @@ class emoji(commands.Cog):
     async def on_message(self, message):
         if '<@!474255126228500480>' in message.content.lower():
             await message.add_reaction('<a:ohh:828312958769692692>')
-        elif '<@!488738167969546272>' in message.content.lower() && message.guild.id == 665842728545943552:
-            await message.add_reaction('<a:ohh:828312958769692692>')
+            
+        elif '<@!488738167969546272>' in message.content.lower():
+            if message.guild.id == 665842728545943552:
+                await message.add_reaction('<a:ohh:828312958769692692>')
 
 
 def setup(bot):
