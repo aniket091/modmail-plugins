630d5ac8816e555509688d66cc44f3bec74ea57f
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-04-04 09:43:27 +0530

    Update ChangeStatus.py

--- ChangeStatus/ChangeStatus.py
+++ ChangeStatus/ChangeStatus.py
@@ -1,65 +1,26 @@
 import discord
-from discord.ext import commands, tasks
-from discord.enums import ActivityType
 import asyncio
+from discord.ext import commands
 
-class ChangeStatus(commands.Cog):
+class utility(commands.Cog):  
     def __init__(self, bot):
         self.bot = bot
-        self.first = None
-        self.second = None
-        self.third = None
+        self.statuss.start()
+    
 
     @tasks.loop(seconds=10)
-    async def start_the_status(self):
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"ANIKET'S SERVER"))
-        await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"!help"))
-        await asyncio.sleep(10)
-        server = self.bot.get_guild(800631529351938089)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} Members!"))
-        await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name=f"Modmail Threads!"))
-        await asyncio.sleep(10)
-
-    @commands.group(name="statusy", invoke_without_command=True)
-    async def status_group(self, ctx):
-        embed = discord.Embed(
-            title="Change the Bot's Status!",
-            description=f"Change the Bot's Status to make it change every 10 Seconds!\n\nAvailable Commands:\n`{self.bot.prefix}statusy start`: Start the Status Changing Process\n`{self.bot.prefix}statusy one`: Set the first status of the bot!\n`{self.bot.prefix}statusy two`: Set the second status of the bot!\n`{self.bot.prefix}statusy three`: Set third first status of the bot!",
-            color=self.bot.main_color
-        )
-        await ctx.send(embed=embed)
-
-    @status_group.command(name="start")
-    async def statusy_start(self, ctx):
-           self.start_the_status.start()
-           await ctx.send("Done! If you experience any problems just run this command again!")
-
-    @status_group.command(name="one")
-    async def first_set(self, ctx, *, first):
-        if first == None:
-            await ctx.send("Please choose something to set!")
-        else:
-            self.first = first
-            await ctx.send(f"Set `{first}` as the first status!")
-
-    @status_group.command(name="two")
-    async def second_set(self, ctx, *, two):
-        if two == None:
-            await ctx.send("Please choose something to set!")
-        else:
-            self.second = two
-            await ctx.send(f"Set `{two}` as the second status!")
-
-    @status_group.command(name="three")
-    async def third_set(self, ctx, *, three):
-        if three == None:
-            await ctx.send("Please choose something to set!")
-        else:
-            self.third = three
-            await ctx.send(f"Set `{three}` as the first status!")
-
+    async def statuss(self):
+      await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"PARTH YT'S SERVER"))  
+      await asyncio.sleep(10)
+      await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"!help"))
+      await asyncio.sleep(10)
+      server = self.bot.get_guild(665842728545943552)
+      await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} Members!"))
+      await asyncio.sleep(10)
+      
+    @statuss.before_loop
+    async def before_statuss(self):
+      await self.bot.wait_until_ready()  
 
 def setup(bot):
-    bot.add_cog(ChangeStatus(bot))
+    bot.add_cog(utility(bot))
